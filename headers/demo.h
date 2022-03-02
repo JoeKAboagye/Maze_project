7d042ca11e95e9b69e7cc0f49f1fb83603b36b8d
JoeKAboagye <josephkwabenaaboagye@gmail.com>
2022-03-02 20:31:33 +0000

    Updates

--- headers/demo.h
+++ headers/demo.h
@@ -11,8 +11,8 @@
 
 typedef struct SDL_Instance
 {
-SDL_Window *window;
-SDL_Renderer *renderer;
+	SDL_Window *window;
+	SDL_Renderer *renderer;
 } SDL_Instance;
 
 int init_instance(SDL_Instance *);
